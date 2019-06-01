f13837687fb800fe84c423c6bfcec95b296d48c4
Daniel Gulic <danielgulic@gmail.com>
2019-06-02 01:08:28 +1000

    file uploading + retrieving

---
+++ server/src/routes/files.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const randomString = require('randomstring');
+const mime = require('mime-types');
+const { mkdirSync } = require('fs');
+const { r } = require('../server');
+const { resolve } = require('path');
+
+const router = (module.exports = express.Router({ mergeParams: true }));
+
+// Direct file request - respond with actual file
+router.get('/', async (req, res) => {
+  const file = (await r
+    .table('files')
+    .filter({ fullName: req.params.file }))[0];
+  console.log(file);
+  if (!file) return res.sendStatus(404);
+  const fileOwner = await r
+    .table('users')
+    .get(file.owner)
+    .run();
+  console.log(fileOwner);
+  console.log(req.hostname);
+  const domain = fileOwner.customDomain || process.env.DEFAULT_DOMAIN;
+  if (req.hostname !== domain) return res.sendStatus(404);
+  res.sendFile(resolve('files/' + fileOwner.id + '/' + req.params.file));
+});
+
+router.use(fileUpload({ preserveExtension: true, safeFileNames: true }));
+
+// Upload file
+router.post('/', async (req, res) => {
+  if (!req.user)
+    return res
+      .status(401)
+      .header({ 'WWW-Authenticate': 'JWT' })
+      .json({ error: 'Not authenticated' });
+  if (req.params.file)
+    return res.status(301).json({ error: 'Use /api/files for POST requests' }); // We don't want people sending POST requests to /:file instead of /api/files
+  if (!req.files) return res.status(400).json({ error: 'No files provided ' });
+  const file = req.files[Object.keys(req.files)[0]];
+  if (file.size > 8e+6) return res.status(400).json({ error: 'Max. upload size is 8 MB' });
+  const shortName = randomString.generate(7);
+  const fileExt = mime.extension(file.mimetype);
+  if (!fileExt) return res.status(400).json({ error: 'Invalid MIME type' });
+  const shortNameWithExt = shortName + '.' + fileExt;
+  try {
+    mkdirSync('files/' + req.user.id);
+  } catch (err) {
+    if (
+      !err
+        .toString()
+        .toLowerCase()
+        .includes('file already exists')
+    )
+      console.error(err);
+  }
+  file.mv(resolve('files/' + req.user.id + '/' + shortNameWithExt));
+  await r
+    .table('files')
+    .insert({
+      name: shortName,
+      fullName: shortName + '.' + fileExt,
+      extension: fileExt,
+      createdAt: Date.now(),
+      owner: req.user.id
+    })
+    .run();
+  res.json({ file: shortNameWithExt });
+});
--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const ejwt = require('express-jwt');
 const fs = require('fs');
+
+try {
+  fs.mkdirSync('files');
+} catch (err) {
+  if (
+    !err
+      .toString()
+      .toLowerCase()
+      .includes('file already exists')
+  )
+    console.error(err);
+}
 
 const r = (exports.r = require('rethinkdbdash')({
   db: process.env.RETHINKDB_DATABASE
@@ -14,12 +27,27 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.enable('trust proxy');
+app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(
-  ejwt({ secret: jwtKey, credentialsRequired: false }),
+  ejwt({
+    secret: jwtKey,
+    credentialsRequired: false,
+    getToken: (req) => {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'JWT'
+      ) {
+        return req.headers.authorization.split(' ')[1];
+      } else if (req.query && req.query.token) {
+        return req.query.token;
+      }
+      return null;
+    }
+  }),
   async (req, res, next) => {
     if (!req.user) return next();
     const id = parseInt(req.user, 10); //ids are stored as numbers
@@ -32,5 +60,7 @@ app.use(
 );
 
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/files', require('./routes/files'));
+app.use('/:file', require('./routes/files'));
 
 app.listen(port, () => console.log('file-host server listening on port', port));
