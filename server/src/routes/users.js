f37d7f5b5ff2b196aedf51113f680794968e43f6
Daniel Gulic <danielgulic@gmail.com>
2019-06-02 13:30:53 +1000

    PATCH /api/users/me

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { r } = require('../server');
-const { ensureOfficialDomain } = require('../util');
+const { ensureOfficialDomain, DOMAIN_NAME_REGEX } = require('../util');
 
 const router = (module.exports = express.Router());
 
@@ -11,4 +11,25 @@ router.get('/me', ensureOfficialDomain, async (req, res) => {
       .header({ 'WWW-Authenticate': 'JWT' })
       .json({ error: 'Not authenticated' });
   res.json({ user: req.user });
+});
+
+router.patch('/me', ensureOfficialDomain, async (req, res) => {
+  if (!req.user)
+    return res
+      .status(401)
+      .header({ 'WWW-Authenticate': 'JWT' })
+      .json({ error: 'Not authenticated' });
+  if (!req.body.customDomain)
+    return res.status(400).json({ error: "Missing property 'customDomain'" });
+  const customDomain = req.body.customDomain.trim();
+  if (!DOMAIN_NAME_REGEX.test(customDomain))
+    return res
+      .status(400)
+      .json({ error: "'customDomain' must be a valid domain name" });
+  const usersWithDomain = await r.table('users').filter({ customDomain });
+  if (usersWithDomain.length > 0) return res.status(403).json({ error: 'A user with this domain name already exists' });
+  await r.table('users').get(req.user.id).update({
+    customDomain
+  });
+  res.sendStatus(204);
 });
--- server/src/util.js
+++ server/src/util.js
@@ -1,7 +1,8 @@
-const PORT_REGEX = /((?::))(?:[0-9]+)$/;
+exports.PORT_REGEX = /((?::))(?:[0-9]+)$/;
+exports.DOMAIN_NAME_REGEX = /^(?=.{0,253}$)(([a-z0-9_][a-z0-9_-]{0,61}[a-z0-9_]|[a-z0-9_])\.)+((?=.*[^0-9])([a-z0-9][a-z0-9-]{0,61}[a-z0-9]|[a-z0-9]))$/i;
 
 exports.ensureOfficialDomain = (req, res, next) => {
-  let officialDomain = process.env.API_BASE.replace(PORT_REGEX, '');
+  let officialDomain = process.env.API_BASE.replace(exports.PORT_REGEX, '');
   officialDomain = officialDomain.replace('https://', '');
   officialDomain = officialDomain.replace('http://', '');
   if (req.hostname !== officialDomain) return res.sendStatus(404);
