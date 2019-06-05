5e0a7102cb595012d696ed60edb0144715ba740f
Daniel Gulic <danielgulic@gmail.com>
2019-06-05 14:47:32 +1000

    add auth functionality to dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,15 +1,48 @@
 import { h, Component } from 'preact';
-// import '../styles/pages/dashboard';
 import { route } from 'preact-router';
 import { API_BASE } from '../config';
+import DashboardProfileCard from '../components/dashboard/DashboardProfileCard';
+import DashboardAccountSettings from '../components/dashboard/DashboardAccountSettings';
 
 export default class Dashboard extends Component {
   state = {
-    loggedIn: !!window.localStorage.getItem('token')
+    user: null
   };
 
-  componentDidMount() {}
+  async componentDidMount() {
+    const res = await fetch(API_BASE + '/api/users/me', {
+      headers: {
+        Authorization: `JWT ${window.localStorage.getItem('token').trim()}`
+      }
+    });
+    const json = await res.json();
+    this.setState({ user: json.user });
+    console.log(this.state.user);
+  }
+
   render() {
-    return <div>hi dashboard</div>;
+    return (
+      <div>
+        {this.state.user && (
+          <div>
+            <p>
+              You're logged in as <b>{this.state.user.username}</b>
+              <br />
+              <a class='dark bold'>Logout</a>
+            </p>
+            <div>
+              <grid columns='2'>
+                <c>
+                  <DashboardProfileCard user={this.state.user} />
+                </c>
+                <c>
+                  <DashboardAccountSettings user={this.state.user} />
+                </c>
+              </grid>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
