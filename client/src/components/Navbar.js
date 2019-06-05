7b0461b28e04d16d57d46fa41e1b04161c47feb3
Daniel Gulic <danielgulic@gmail.com>
2019-06-05 14:47:01 +1000

    modify navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,17 +4,8 @@ import { Link } from 'preact-router';
 export default class Navbar extends Component {
   render() {
     return (
-      <div class='nav'>
-        <h5 class='nav-logo'>File Host</h5>
-        {/* <Link class='nav-item' href='/dashboard'>
-          Dashboard
-        </Link> */}
-        {/* <a class='nav-item' href='#'>
-          Item
-        </a>
-        <a class='nav-item' href='#'>
-          Item
-        </a> */}
+      <div class='navbar'>
+        <h2 class='title'>File Host</h2>
       </div>
     );
   }
