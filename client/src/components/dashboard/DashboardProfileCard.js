ad0006088df5a89c0ed054c3304d823d5ef9187a
Daniel Gulic <danielgulic@gmail.com>
2019-06-05 14:48:07 +1000

    dashboard readonly profile card

---
+++ client/src/components/dashboard/DashboardProfileCard.js
@@ -0,0 +1,36 @@
+import { h, Component } from 'preact';
+
+export default class DashboardProfileCard extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.user && (
+          <div>
+            <h4>My Profile</h4>
+            <div class='card'>
+              <div>
+                <div>
+                  <label>Username</label>
+                  <input
+                    type='text'
+                    disabled
+                    value={this.props.user.username}
+                  />
+                </div>
+                <div>
+                  <label>Email</label>
+                  <input
+                    class='input-with-icon'
+                    type='text'
+                    disabled
+                    value={this.props.user.email}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
