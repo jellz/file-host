866987ecfdfe534d4f4bda0521c75bbc9b5ee692
Daniel Gulic <danielgulic@gmail.com>
2019-06-07 20:39:10 +1000

    remove cors module

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const ejwt = require('express-jwt');
@@ -27,10 +26,18 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.enable('trust proxy');
-app.use(helmet());
-app.use(cors());
 app.use(morgan('dev'));
+app.use(helmet());
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
+  next();
+});
 
 app.use(
   ejwt({
